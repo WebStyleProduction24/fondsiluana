edab63ed75fcd73cc358a2e1956d3661551263f4
Jack36VRN <jack3@JACK36-LAPTOP>
2019-04-22 18:33:33 +0300

    Update 2019/04/22 18:32 +3GMT
    Подключены библиотеки bootstrap-4 посредством CDN

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
 </head>
