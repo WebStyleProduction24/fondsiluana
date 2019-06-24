688b1bb9c2019e9828d06b99a5349b7681d28dea
Jack36VRN <jack3@JACK36-LAPTOP>
2019-06-24 15:38:28 +0300

    Update 2019/06/24 15:38 +3GMT

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function enqueue_styles() {
 	wp_enqueue_style( 'whitesquare-style', get_stylesheet_uri());
-	wp_register_style('font-style', 'http://fonts.googleapis.com/css?family=Oswald:400,300');
+	wp_register_style('font-style', 'https://fonts.googleapis.com/css?family=Oswald:400,300');
 	wp_enqueue_style( 'font-style');
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
