424c185c0907386837d25b691dae47c3ea8f2a24
Jack36VRN <36456429+Jack3@users.noreply.github.com>
2019-09-19 21:25:28 +0300

    Update home.php

--- home.php
+++ home.php
@@ -21,71 +21,85 @@
 			<img class="position-absolute bcg-afonskiy-2" src="<?php echo get_template_directory_uri(); ?>/img/bcg-afonskiy-2.png" alt="преп. Силуан Афонский">
 
 		</div>
-
 	</div>
-</section>
 
+</section>
 <section id="help" class="container">
+
+
 	<div class="row">
-		<div class="col-12"><h3>Им нужна помощь</h3></div>
-		<div class="col-lg-4">
-			<a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" title="Cбор средств на покупку неонатальных термокроваток"><img class="w-100" src="<?php echo get_template_directory_uri(); ?>/img/krovatka.png" alt="Неонатальные термокроватки"></a>
-		</div>
-		<div class="col-lg-4">
-			<h4 class="gold-line-tl" style="line-height: 35px;">Cбор средств на покупку неонатальных термокроваток</h4>
-			<p>Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»).</p>
-			<p class="text-right"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
-		</div>
-		<div class="col-lg-4">
-			<div class="collected mt-5">
-				<p class="mb-1">Собрано пожертвований</p>
-				<p class="mb-1 money">1000 из 307&nbsp;600 руб. </p>
+		<div class="col-12">
+			<div class="row">
+				<div class="col-9">
+					<h3>Им нужна помощь</h3>
+					<div class="row">
+						<div class="col-lg-4">
+							<a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" title="Cбор средств на покупку неонатальных термокроваток"><img class="w-100" src="<?php echo get_template_directory_uri(); ?>/img/krovatka.png" alt="Неонатальные термокроватки"></a>
+						</div>
+						<div class="col-lg-4">
+							<h4 class="gold-line-tl" style="line-height: 35px;">Cбор средств на покупку неонатальных термокроваток</h4>
+							<p>Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»).</p>
+							<p class="text-right"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
+						</div>
+						<div class="col-lg-4">
+							<div class="collected mt-5">
+								<p class="mb-1">Собрано пожертвований</p>
+								<p class="mb-1 money">1000 из 307&nbsp;600 руб. </p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<h3>Им помогли</h3>
+					<a href="/pervyj-sbor-sredstv/" title="София и Меркурий"><img class="w-100" src="<?php echo get_template_directory_uri(); ?>/img/sofiya-merkuriy.jpg" alt="Софья и Меркурий"></a>
+					<h4 class="gold-line-tl">София и Меркурий</h4>
+					<p>Острый лимфобластный лейкоз (рак крови)</p>
+					<p class="text-right"><a href="/pervyj-sbor-sredstv/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
+					<div class="collected mt-5">
+						<p class="mb-1">Сбор пожертвований</p>
+						<p class="mb-1 money">окончен</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
-<section id="helped" class="container">
-	<div class="row">
-		<div class="col-12"><h3>Им помогли</h3></div>
-		<div class="col-lg-4">
-			<a href="/pervyj-sbor-sredstv/" title="София и Меркурий"><img class="w-100" src="<?php echo get_template_directory_uri(); ?>/img/sofiya-merkuriy.jpg" alt="Софья и Меркурий"></a>
-		</div>
-		<div class="col-lg-4">
-			<h4 class="gold-line-tl">София и Меркурий</h4>
-			<p>Острый лимфобластный лейкоз (рак крови)</p>
-			<p class="text-right"><a href="/pervyj-sbor-sredstv/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
-		</div>
-		<div class="col-lg-4">
-			<div class="collected mt-5">
-				<p class="mb-1">Сбор пожертвований</p>
-				<p class="mb-1 money">окончен</p>
+
+
+
+
+
+
+
+
+
+
+
+
+		<section id="news" class="container">
+			<div class="row">
+				<div class="col-12"><h3>Новости Фонда</h3></div>
+
+				<div class="col-lg-3">
+					<h5 class="gold-line-bottom">Сбор средств на покупку неонатальных термокроваток.</h5>
+					<p>Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»)</p>
+					<p class="text-right"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
+				</div>
+				<div class="col-lg-3">
+					<h5 class="gold-line-bottom">Милостью Божьей мы помогли Софии.</h5>
+					<p>Спасибо огромное за пожертвования. Операция прошла успешно, София идёт на поправку.</p>
+					<p class="text-right"><a href="/pervyj-sbor-sredstv/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
+				</div>
+				<div class="col-lg-3">
+					<h5 class="gold-line-bottom">Регистрация фонда</h5>
+					<p>Дорогие друзья, наконец-то, спустя почти полтора года непрерывных усилий, с третьей попытки нам удалось с Божьей помощью зарегистрировать долгожданный благотворительный фонд!</p>
+					<p class="text-right"><a href="#" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
+				</div>
 			</div>
 		</div>
 	</div>
-</section>
-
-
-<section id="news" class="container">
-	<div class="row">
-		<div class="col-12"><h3>Новости Фонда</h3></div>
-		
-		<div class="col-lg-3">
-			<h5 class="gold-line-bottom">Сбор средств на покупку неонатальных термокроваток.</h5>
-			<p>Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»)</p>
-			<p class="text-right"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
-		</div>
-		<div class="col-lg-3">
-			<h5 class="gold-line-bottom">Милостью Божьей мы помогли Софии.</h5>
-			<p>Спасибо огромное за пожертвования. Операция прошла успешно, София идёт на поправку.</p>
-			<p class="text-right"><a href="/pervyj-sbor-sredstv/" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
-		</div>
-		<div class="col-lg-3">
-			<h5 class="gold-line-bottom">Регистрация фонда</h5>
-			<p>Дорогие друзья, наконец-то, спустя почти полтора года непрерывных усилий, с третьей попытки нам удалось с Божьей помощью зарегистрировать долгожданный благотворительный фонд!</p>
-			<p class="text-right"><a href="#" class="button border-bottom gold-border pb-1 link-gray">Подробнее</a></p>
-		</div>
-	</div>
+</div>
 </section>
 
 
