02caced0d27bde319241fa269d26ff19008328e0
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2019-11-06 13:43:49 +0300

    Update 2019/11/06 13:43 +3GMT
    Готова верстка страницы "новости"

---
+++ news.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Template Name: Шаблон страницы "Новости"
+*/
+?>
+
+
+
+
+
+<?php get_header(); ?>
+<!-- 
+
+-->
+
+
+
+
+<section>
+  <div class="container">
+
+    <div class="row">
+      <!-- Основной блок -->
+      <div class="col-lg-8 col-md-12">
+        <h2 class="mb-3 mb-sm-5">Объявлен сбор средств на покупку неонатальных термокроваток</h2>
+        <div class="content text-right gold-line-tr position-relative py-2 pb-4 a">27 Июля 2019</div>
+        <img src="/wp-content/uploads/2019/07/krovatka.png" alt="Неонатальные термокроватки" class="w-100">
+        <div class="content fs-14 py-4">
+          <p>
+            Дорогие друзья! Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»). Кроватки нужны для поддержания температурного климата вокруг деток и имеют возможность подключения дополнительного оборудования для проведения терапий и процедур деткам.
+          </p>
+          <p>
+            Цена одной такой кроватки 153800 ₽. Деток в отделении много и подобных кроваток не хватает, но давайте попробуем вместе собрать средства хотя бы на пару таких кроваток. Если сможем больше – будет замечательно.
+          </p>
+          <p>
+            Напоминаем, что по результатам сбора мы обязательно выложим отчёт о проделанной работе.
+          </div>
+        </div>
+
+
+        <!-- Блок новости -->
+        <div class="col-lg-4 col-md-12 news-sidebar">
+          <h3 class="mb-3">Новости Фонда</h3>
+          <div class="row">
+
+            <div class="col-lg-12 col-md-4 position-relative mb-lg-4 mb-4 mb-sm-2">
+              <div class="row news__title-block">
+                <div class="col-8 col-lg-9 d-table-cell align-middle pl-0">
+                  <h5 class="gold-line-bottom">Сбор средств на покупку неонатальных термокроваток</h5>
+                </div>
+                <div class="col-4 col-lg-3 d-table-cell align-middle new-date">
+                  <span>19</span>
+                  <br>ноября 
+                </div>
+              </div>
+              <p class="pb-lg-4 mt-3 mb-md-0 mb-2 news__content">
+                Организован сбор средств на покупку неонатальных термокроваток для грудных детей, от которых отказались родители, в отделение недоношенных детей Воронежской областной детской клинической больницы № 1 (БУЗ ВО «ВОДКБ № 1»)
+              </p>
+              <p class="text-right w-100 news__button">
+                <a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">
+                  Подробнее
+                </a>
+              </p>
+            </div>
+
+            <div class="col-lg-12 col-md-4 position-relative mb-lg-4 mb-4 mb-sm-2">
+              <div class="row news__title-block">
+                <div class="col-8 col-lg-9 d-table-cell align-middle pl-0">
+                  <h5 class="gold-line-bottom">Милостью Божьей мы помогли Софии</h5>
+                </div>
+                <div class="col-4 col-lg-3 d-table-cell align-middle new-date">
+                  <span>22</span>
+                  <br>марта
+                </div>
+              </div>
+              <p class="pb-lg-4 mt-3 mb-md-0 mb-2 news__content">
+                Спасибо огромное за пожертвования. Операция прошла успешно, София идёт на поправку.
+              </p>
+              <p class="text-right w-100">
+                <a href="/pervyj-sbor-sredstv/" class="button border-bottom gold-border pb-1 link-gray">
+                  Подробнее
+                </a>
+              </p>
+            </div>
+
+            <div class="col-lg-12 col-md-4 position-relative mb-lg-4 mb-4 mb-sm-2">
+             <div class="row news__title-block">
+              <div class="col-8 col-lg-9 d-table-cell align-middle pl-0">
+                <h5 class="gold-line-bottom">Регистрация фонда</h5>
+              </div>
+              <div class="col-4 col-lg-3 d-table-cell align-middle new-date">
+                <span>31</span>
+                <br>июля
+              </div>
+            </div>
+            <p class="pb-lg-4 mt-3 mb-md-0 mb-2 news__content">
+              Дорогие друзья, наконец-то, спустя почти полтора года непрерывных усилий, с третьей попытки нам удалось с Божьей помощью зарегистрировать долгожданный благотворительный фонд!
+            </p>
+            <p class="text-right w-100">
+              <a href="#" class="button border-bottom gold-border pb-1 link-gray">
+                Подробнее
+              </a>
+            </p>
+          </div>
+
+        <!-- <div class="col-lg-12 col-md-4 position-relative mb-lg-4 mb-4 d-md-none d-lg-block">
+          <div class="row news__title-block">
+            <div class="col-8 col-lg-9 pl-0 d-table-cell align-middle">
+              <h5 class="gold-line-bottom">Регистрация фонда</h5>
+            </div>
+            <div class="col-4 col-lg-3 d-table-cell new-date align-middle">
+              <span>27</span>
+              <br>января 
+            </div>
+          </div>
+          <p class="pb-lg-4 mt-3 mb-md-0 mb-2 news__content">
+            Дорогие друзья, наконец-то, спустя почти полтора года непрерывных усилий, с третьей попытки нам удалось с Божьей помощью зарегистрировать долгожданный благотворительный фонд!
+          </p>
+          <p class="text-right w-100">
+            <a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="button border-bottom gold-border pb-1 link-gray">
+              Подробнее
+            </a>
+          </p>
+        </div> -->
+
+
+      </div>
+
+
+
+
+    </div>
+  </div>
+
+</div>
+</section>
+
+
+<?php get_footer(); ?>
