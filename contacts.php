f2b6bfdaeb81ea842e25057fc070c60708da796d
westerovs <westerovs@mail.ru>
2019-11-08 19:10:22 +0300

    предфинальные правки контакты
    08,11,2019

--- contacts.php
+++ contacts.php
@@ -13,31 +13,26 @@ Template Name: Шаблон страницы "Контакты"
 
 
 
-
-
-
-
 <div class="contacts-wrapper">
 
-  <div class="container">
+  <div class="container mb-5">
     <div class="row">
-      <!-- Главная колонка -->
-      <div class="col-lg-9 col-12">
 
-        <div class="container contacts">
+      <!-- Главная колонка -->
+      <div class="col-lg-8 col-md-12 pb-5">
+        <div class="container contacts mb-3">
           <div class="row">
-            <h2 class="contacts-h2 w-100">Наши Контакты</h2>
-            <ul class="list-group">
-              <li class="list-group-item">
+            <h2 class="contacts-h2 w-100 mb-4">Наши Контакты</h2>
+              <div class="col-md-4 mb-5">
                 <div class="contacts-block">
                   <div class="contacts-block-top">
                     <p class="size20 gold-line-bottom">адрес</p>
                   </div>
-                  <b class="contacts-b">г. Воронеж</b>
+                  <a class="link-gray contacts-link" href="#maps" title="перейти к карте">г. Воронеж</a>
                   <p class="size0_9">ул. Коммисаржевской 10, офис 123</p>
                 </div>
-              </li>
-              <li class="list-group-item">
+              </div>
+              <div class="col-md-4 mb-5">
                 <div class="contacts-block">
                   <div class="contacts-block-top">
                     <p class="size20 gold-line-bottom">телефон</p>
@@ -46,8 +41,8 @@ Template Name: Шаблон страницы "Контакты"
                     +7 650 106 57 31
                   </a>
                 </div>
-              </li>
-              <li class="list-group-item">
+              </div>
+              <div class="col-md-4 mb-5">
                 <div class="contacts-block">
                   <div class="contacts-block-top gold-line-bottom">
                     <p class="size20">e-mail</p>
@@ -56,82 +51,75 @@ Template Name: Шаблон страницы "Контакты"
                     76501065731@yandex.ru
                   </a>
                 </div>
-              </li>
-            </ul>
+              </div>
           </div>
         </div>
 
-        <div class="container">
           <div class="row">
             <!-- для партнёров -->
             <div class="col-md-6 partner">
-              <h3 class="contacts-h3 w-100">Для партнёров</h3>
-              <ul class="list-group flex-column">
-                <li class="list-group-item">
-                  <div class="contacts-block">
-                    <div class="contacts-block-top">
-                      <p class="size20 gold-line-bottom">телефон</p>
-                    </div>
-                    <a class="link-gray contacts-link" href="tel:+7(650)1065731">
-                      +7 650 106 57 31
-                    </a>
+              <h3 class="contacts-h3 w-100 mb-4">Для партнёров</h3>
+              <div class="col-md-12 mb-5">
+                <div class="contacts-block">
+                  <div class="contacts-block-top">
+                    <p class="size20 gold-line-bottom">телефон</p>
                   </div>
-                </li>
-                <li class="list-group-item">
-                  <div class="contacts-block">
-                    <div class="contacts-block-top gold-line-bottom">
-                      <p class="size20">e-mail</p>
-                    </div>
-                    <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
-                      76501065731@yandex.ru
-                    </a>
+                  <a class="link-gray contacts-link" href="tel:+7(650)1065731">
+                    +7 650 106 57 31
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-12 mb-5">
+                <div class="contacts-block">
+                  <div class="contacts-block-top gold-line-bottom">
+                    <p class="size20">e-mail</p>
                   </div>
-                </li>
-              </ul>
+                  <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
+                    76501065731@yandex.ru
+                  </a>
+                </div>
+              </div>
             </div>
 
             <!-- для волонтёров -->
             <div class="col-md-6 volunteer">
-              <h3 class="contacts-h3 w-100">Для волонтёров</h3>
+              <h3 class="contacts-h3 w-100 mb-4">Для волонтёров</h3>
               <form action="">
                 <div class="form-group m-0">
-                  <input id="inp1" class="form-control border-input"  type="text" placeholder="Ваше имя">
+                  <input id="inp1" class="form-control border-input"  type="text" placeholder="Ваше имя" required>
                   <label for="inp1"></label>
                 </div>
                 <div class="form-group m-0">
-                  <input id="inp2" class="form-control border-input"  type="text" placeholder="Контактный телефон">
+                  <input id="inp2" class="form-control border-input"  type="text" placeholder="Контактный телефон" required>
                   <label for="inp2"></label>
                 </div>
                 <div class="form-group m-0">
-                  <input id="inp3" class="form-control border-input"  type="text" placeholder="Ваш e-mail">
+                  <input id="inp3" class="form-control border-input"  type="text" placeholder="Ваш e-mail" required>
                   <label for="inp3"></label>
                 </div>
-                <div class="form-group m-0">
-                  <textarea class="form-control border-textarea" id="textarea" rows="3" placeholder="Вашe сообщение"></textarea>
+                <div class="form-group mb-3">
+                  <textarea class="form-control border-textarea" id="textarea" rows="3" placeholder="Вашe сообщение" required></textarea>
                   <label for="textarea"></label>
                 </div>
-                <div class="form-group m-0 form-check">
+                <div class="form-group mb-3 form-check gold-line-tl">
                   <input id="inp4" class="form-check-input"  type="checkbox" checked>
-                  <label class="form-check-label label-font" for="inp4">Согласие на обработку персональных данных</label>
+                  <label class="form-check-label label-font " for="inp4">Согласие на обработку персональных данных</label>
                 </div>
-
-                <a href="#" class="btn btn-red fs-10 beneficence" role="button" aria-disabled="true">
+                <a href="#" class="btn btn-red fs-10 beneficence d-block m-auto" role="button" aria-disabled="true">
                   Стать волонтёром
                 </a>
-
               </form>
             </div>
           </div>
         </div>
-      </div><!-- закрыв. col-9 -->
-      
+
 
 
 
 
       <!-- колонка реквизиты -->
-      <div class="col-lg-3 col-auto requisites">
-        <h4 class="contacts-h4 w-100">Реквизиты фонда</h4>
+      <div class="col-lg-3 offset-lg-1 col-md-6 requisites">
+        <h4 class="contacts-h4 w-100 gold-line-tl">Реквизиты фонда</h4>
         <p class="requisites-text">"Фонд св. преп. Силуана Афонского"</p>
         <p class="requisites-text">
           <span class="requisites-text-header">Номер счёта:</span>
@@ -139,7 +127,6 @@ Template Name: Шаблон страницы "Контакты"
         </p>
         <p class="requisites-text">
           <span class="requisites-text-header">Банк получателя:</span>
-          
           ЦЕНТРАЛЬНО-ЧЕРНОЗЕМНЫЙ БАНК ПАО СБЕРБАНК
         </p>
         <p class="requisites-text">
@@ -163,22 +150,35 @@ Template Name: Шаблон страницы "Контакты"
           366401001
         </p>
         <div class="text-center">
-          <a href="#" class="btn requisites-btn fs-10 " role="button" aria-disabled="true">
+          <a href="#" class="btn fs-10 white-beneficence d-block m-auto" role="button" aria-disabled="true">
             Скачать реквизиты
           </a>
         </div>
       </div>
+      <div class="d-lg-none col-md-6">
+        <div class="map-vertical">
+          <a name="maps">
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af76b76def688c88934c0d976dfbb8cfc00f020fd048dec3e18c2008250c9c541&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
+          </a>
+        </div>
+      </div>
 
-    </div><!-- закрыв. гл. row-->
-  </div><!-- закрыв. гл. container -->
-</div><!-- wrapper -->
+    </div>
+  </div>
+</div><!--конец wrapper -->
 
 
 
-<!-- карта -->
-<div class="container map">
+
+
+<!-- карта для большой версии -->
+<div class="container bottom-map" >
   <div class="row">
-    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abd4437fc1983328b3ffe024a7e5ade24658ad904d2f529dabb62617b4cf7ea57&amp;source=constructor" width="1140" height="300" frameborder="0"></iframe>
+    <div class="col-lg-12 p-0">
+      <a >
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abd4437fc1983328b3ffe024a7e5ade24658ad904d2f529dabb62617b4cf7ea57&amp;source=constructor" width="100%" height="300" frameborder="0"></iframe>
+      </a>
+    </div>
   </div>
 </div>
 
