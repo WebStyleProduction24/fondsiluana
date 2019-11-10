17bb6203fee7ba365c3e26bc382586a123b9003b
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2019-11-10 23:30:26 +0300

    Update 2019/11/10 23:30 +3GMT

--- contacts.php
+++ contacts.php
@@ -12,172 +12,174 @@ Template Name: Шаблон страницы "Контакты"
 
 
 
+<div class="container">
+  <div class="row">
 
-<div class="contacts-wrapper">
-
-  <div class="container mb-5">
-    <div class="row">
-
-      <!-- Главная колонка -->
-      <div class="col-lg-8 col-md-12 pb-5">
-        <div class="container contacts mb-3">
-          <div class="row">
-            <h2 class="contacts-h2 w-100 mb-4">Наши Контакты</h2>
-              <div class="col-md-4 mb-5">
-                <div class="contacts-block">
-                  <div class="contacts-block-top">
-                    <p class="size20 gold-line-bottom">адрес</p>
-                  </div>
-                  <a class="link-gray contacts-link" href="#maps" title="перейти к карте">г. Воронеж</a>
-                  <p class="size0_9">ул. Коммисаржевской 10, офис 123</p>
-                </div>
+    <!-- Главная колонка -->
+    <div class="col-lg-8 col-md-12">
+      <div class="container">
+        <div class="row">
+          <h2 class="contacts-h2 w-100 mb-4">Наши Контакты</h2>
+          <div class="col-md-4 mb-5">
+            <div class="contacts-block">
+              <div class="contacts-block-top">
+                <p class="f-12 pb-2 mb-2 gold-line-bottom">адрес</p>
               </div>
-              <div class="col-md-4 mb-5">
-                <div class="contacts-block">
-                  <div class="contacts-block-top">
-                    <p class="size20 gold-line-bottom">телефон</p>
-                  </div>
-                  <a class="link-gray contacts-link" href="tel:+7(650)1065731">
-                    +7 650 106 57 31
-                  </a>
-                </div>
+              <a class="link-gray contacts-link" href="#maps" title="перейти к карте">г. Воронеж</a>
+              <p class="size0_9">ул. Коммисаржевской 10, офис 123</p>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="contacts-block">
+              <div class="contacts-block-top">
+                <p class="f-12 pb-2 mb-2 gold-line-bottom">телефон</p>
               </div>
-              <div class="col-md-4 mb-5">
-                <div class="contacts-block">
-                  <div class="contacts-block-top gold-line-bottom">
-                    <p class="size20">e-mail</p>
-                  </div>
-                  <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
-                    76501065731@yandex.ru
-                  </a>
-                </div>
+              <a class="link-gray contacts-link" href="tel:+7(650)1065731">
+                +7 650 106 57 31
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="contacts-block">
+              <div class="contacts-block-top">
+                <p class="f-12 pb-2 mb-2 gold-line-bottom">e-mail</p>
               </div>
+              <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
+                76501065731@yandex.ru
+              </a>
+            </div>
           </div>
         </div>
+      </div>
 
-          <div class="row">
-            <!-- для партнёров -->
-            <div class="col-md-6 partner">
-              <h3 class="contacts-h3 w-100 mb-4">Для партнёров</h3>
-              <div class="col-md-12 mb-5">
-                <div class="contacts-block">
-                  <div class="contacts-block-top">
-                    <p class="size20 gold-line-bottom">телефон</p>
-                  </div>
-                  <a class="link-gray contacts-link" href="tel:+7(650)1065731">
-                    +7 650 106 57 31
-                  </a>
-                </div>
+      <div class="row">
+        <!-- для партнёров -->
+        <div class="col-md-6 partner">
+          <h3 class="contacts-h3 w-100 mb-4">Для партнёров</h3>
+          <div class="col-md-12 mb-5">
+            <div class="contacts-block">
+              <div class="contacts-block-top">
+                <p class="f-12 pb-2 mb-2 gold-line-bottom">телефон</p>
               </div>
-              <div class="col-md-12 mb-5">
-                <div class="contacts-block">
-                  <div class="contacts-block-top gold-line-bottom">
-                    <p class="size20">e-mail</p>
-                  </div>
-                  <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
-                    76501065731@yandex.ru
-                  </a>
-                </div>
-              </div>
+              <a class="link-gray contacts-link" href="tel:+7(650)1065731">
+                +7 650 106 57 31
+              </a>
             </div>
-
-            <!-- для волонтёров -->
-            <div class="col-md-6 volunteer">
-              <h3 class="contacts-h3 w-100 mb-4">Для волонтёров</h3>
-              <form action="">
-                <div class="form-group m-0">
-                  <input id="inp1" class="form-control border-input"  type="text" placeholder="Ваше имя" required>
-                  <label for="inp1"></label>
-                </div>
-                <div class="form-group m-0">
-                  <input id="inp2" class="form-control border-input"  type="text" placeholder="Контактный телефон" required>
-                  <label for="inp2"></label>
-                </div>
-                <div class="form-group m-0">
-                  <input id="inp3" class="form-control border-input"  type="text" placeholder="Ваш e-mail" required>
-                  <label for="inp3"></label>
-                </div>
-                <div class="form-group mb-3">
-                  <textarea class="form-control border-textarea" id="textarea" rows="3" placeholder="Вашe сообщение" required></textarea>
-                  <label for="textarea"></label>
-                </div>
-                <div class="form-group mb-3 form-check gold-line-tl">
-                  <input id="inp4" class="form-check-input"  type="checkbox" checked>
-                  <label class="form-check-label label-font " for="inp4">Согласие на обработку персональных данных</label>
-                </div>
-                <a href="#" class="btn btn-red fs-10 beneficence d-block m-auto" role="button" aria-disabled="true">
-                  Стать волонтёром
-                </a>
-              </form>
+          </div>
+          <div class="col-md-12 mb-5">
+            <div class="contacts-block">
+              <div class="contacts-block-top gold-line-bottom">
+                <p class="f-12 pb-2 mb-2">e-mail</p>
+              </div>
+              <a class="link-gray contacts-link" href="mailto:76501065731@yandex.ru">
+                76501065731@yandex.ru
+              </a>
             </div>
           </div>
         </div>
 
-
-
-
-
-      <!-- колонка реквизиты -->
-      <div class="col-lg-3 offset-lg-1 col-md-6 requisites">
-        <h4 class="contacts-h4 w-100 gold-line-tl">Реквизиты фонда</h4>
-        <p class="requisites-text">"Фонд св. преп. Силуана Афонского"</p>
-        <div>
-          Номер счёта:
-          <p>40703810513000001134</p>
-        </div>
-        <div>
-          Банк получателя:
-          <p>ЦЕНТРАЛЬНО-ЧЕРНОЗЕМНЫЙ БАНК ПАО СБЕРБАНК</p>
-        </div>
-        <div>
-          БИК:
-          <p>042007681</p>
-        </div>
-        <div>
-          Кор. счёт:
-          <p>30101810600000000681</p>
-        </div>
-        <div>
-          ИНН:
-          <p>3664238750</p>
-        </div>
-        <div>
-          ОГРН:
-          <p>1183600000924</p>
-        </div>
-        <div>
-          КПП:
-          <p>366401001</p>
-        </div>
-        <div class="text-center">
-          <a href="#" class="btn fs-10 white-beneficence d-block m-auto" role="button" aria-disabled="true">
-            Скачать реквизиты
-          </a>
+        <!-- для волонтёров -->
+        <div class="col-md-6 volunteer">
+          <h3 class="contacts-h3 w-100 mb-4">Для волонтёров</h3>
+              <div class="contacts-block-top">
+                <p class="f-12 pb-2 mb-2 gold-line-bottom">напишите нам</p>
+              </div>
+          <p class="mt-4">Мы всегда рады новым помощникам! Напишите нам и с вами свяжется наш координатор волонтеров.</p>
+          <form action="">
+            <div class="form-group m-0">
+              <input id="inp1" class="form-control border-input"  type="text" placeholder="Ваше имя" required>
+              <label for="inp1"></label>
+            </div>
+            <div class="form-group m-0">
+              <input id="inp2" class="form-control border-input"  type="text" placeholder="Контактный телефон" required>
+              <label for="inp2"></label>
+            </div>
+            <div class="form-group m-0">
+              <input id="inp3" class="form-control border-input"  type="text" placeholder="Ваш e-mail" required>
+              <label for="inp3"></label>
+            </div>
+            <div class="form-group mb-3">
+              <textarea class="form-control border-textarea" id="textarea" rows="3" placeholder="Вашe сообщение" required></textarea>
+              <label for="textarea"></label>
+            </div>
+            <div class="form-group mb-3 form-check gold-line-tl">
+              <input id="inp4" class="form-check-input"  type="checkbox" checked>
+              <label class="form-check-label label-font " for="inp4">Согласие на обработку персональных данных</label>
+            </div>
+            <div class="col-12 position-relative">
+              <a href="#" class="w-100 text-white hrt-btn fs-10 btn" tabindex="-1" role="button" aria-disabled="true">
+                Стать волонтёром
+              </a>
+            </div>
+          </form>
         </div>
       </div>
-      <div class="d-lg-none col-md-6">
-        <div class="map-vertical">
-          <a name="maps">
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af76b76def688c88934c0d976dfbb8cfc00f020fd048dec3e18c2008250c9c541&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
-          </a>
-        </div>
-      </div>
-
     </div>
+
+
+
+
+
+    <!-- колонка реквизиты -->
+    <div class="col-lg-3 col-md-6 requisites fs-12 pt-4">
+      <h4 class="text-left gold-line-top position-relative pt-4">Реквизиты фонда</h4>
+      <p class="requisites-text">"Фонд св. преп. Силуана Афонского"</p>
+      <div>
+        Номер счёта:
+        <p>40703810513000001134</p>
+      </div>
+      <div>
+        Банк получателя:
+        <p>ЦЕНТРАЛЬНО-ЧЕРНОЗЕМНЫЙ БАНК ПАО СБЕРБАНК</p>
+      </div>
+      <div>
+        БИК:
+        <p>042007681</p>
+      </div>
+      <div>
+        Кор. счёт:
+        <p>30101810600000000681</p>
+      </div>
+      <div>
+        ИНН:
+        <p>3664238750</p>
+      </div>
+      <div>
+        ОГРН:
+        <p>1183600000924</p>
+      </div>
+      <div>
+        КПП:
+        <p>366401001</p>
+      </div>
+      <a href="#" class="btn position-relative wht-hrt-btn fs-12" role="button" aria-disabled="true">
+        Скачать реквизиты
+      </a>
+    </div>
+    <div class="d-lg-none col-md-6">
+      <div class="map-vertical">
+        <a name="maps">
+          <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af76b76def688c88934c0d976dfbb8cfc00f020fd048dec3e18c2008250c9c541&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
+        </a>
+      </div>
+    </div>
+
   </div>
-</div><!--конец wrapper -->
+</div>
 
 
 
+<script type="text/javascript">
+</script>
 
 
 <!-- карта для большой версии -->
-<div class="container bottom-map" >
-  <div class="row">
-    <div class="col-lg-12 p-0">
-      <a >
-        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abd4437fc1983328b3ffe024a7e5ade24658ad904d2f529dabb62617b4cf7ea57&amp;source=constructor" width="100%" height="300" frameborder="0"></iframe>
-      </a>
+<div class="container" >
+  <div class="row mt-4">
+    <div class="col-lg-12">
+
+      <div id="map"></div>
+
     </div>
   </div>
 </div>
--- header.php
+++ header.php
@@ -4,48 +4,48 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<meta name ="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1" />
 	<meta name="format-detection" content="telephone=no">
 	<meta name="MobileOptimized" content="320">
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" type="image/x-icon"> 
 	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div class="wrapper">
-		<?php
-		if( is_home() || is_page(72) ){}
+	<?php
+	if( is_home() || is_page(72) ){}
 		else { ?>
 			<div class="container">
 
 				<h4 class="attention">Внимание! Страница работает в режиме разработки, приносим извинения за принесенные неудобства</h4>
 
-				</h2>
+			</h2>
+		</div>
+	<?php } ?>
+	<header>
+		<div class="container my-3">
+			<div class="row justify-content-end">
+				<nav class="navbar navbar-expand-lg navbar-light">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<?php wp_nav_menu( [
+						'container_id'		=> 'navbarSupportedContent', 
+						'container_class' => 'collapse navbar-collapse', 
+						'menu_class' 			=> 'navbar-nav mr-auto'
+					] ); ?>
+				</nav>
 			</div>
-		<?php } ?>
-		<header>
-			<div class="container my-3">
-				<div class="row justify-content-end">
-						<nav class="navbar navbar-expand-lg navbar-light">
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<?php wp_nav_menu( [
-							'container_id'		=> 'navbarSupportedContent', 
-							'container_class' => 'collapse navbar-collapse', 
-							'menu_class' 			=> 'navbar-nav mr-auto'
-						] ); ?>
-					</nav>
-				</div>
+		</div>
+		<div class="container mb-5">
+			<div class="row">
+				<div class="col-lg-5 pb-3"><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-main.png" alt="" type="image/png"></a></div>
+				<div class="col-6 col-lg-2 text-center m-auto"><a href="#" class="button border-top gold-border pt-1 link-gray button-menu">Нужна помощь</a></div>
+				<div class="col-6 col-lg-2 text-center m-auto"><a href="#" class="button border-top gold-border pt-1 link-gray button-menu">Стать волонтером</a></div>
+				<div class="col-lg-3 text-center m-auto"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="w-100 text-white roboto btn fs-10 hrt-btn" tabindex="-1" role="button" aria-disabled="true">Сделать доброе дело</a></div>
 			</div>
-			<div class="container mb-5">
-				<div class="row">
-					<div class="col-lg-5 pb-3"><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-main.png" alt="" type="image/png"></a></div>
-					<div class="col-6 col-lg-2 pb-3 text-center m-auto"><a href="#" class="button border-top gold-border pt-1 link-gray button-menu">Нужна помощь</a></div>
-					<div class="col-6 col-lg-2 pb-3 text-center m-auto"><a href="#" class="button border-top gold-border pt-1 link-gray button-menu">Стать волонтером</a></div>
-					<div class="col-lg-3 text-center m-auto"><a href="/campaign/cbor-sredstv-na-pokupku-neonatalnyh-termokrovatok/" class="btn btn-red fs-10 beneficence" tabindex="-1" role="button" aria-disabled="true">Сделать доброе дело</a></div>
-				</div>
-			</div>
-		</header>
+		</div>
+	</header>
+
+	<main>
