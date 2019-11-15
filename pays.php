b72a9eb5c83867b308435c65980d02bfd34593d7
etozhejuicy <57537080+etozhejuicy@users.noreply.github.com>
2019-11-15 16:01:02 +0300

    last night
    pays.php & vasiliev.css has been update

--- pays.php
+++ pays.php
@@ -10,65 +10,90 @@ Template Name: Шаблон Формы оплаты
 
 <?php get_header(); ?>
 
+<link rel="stylesheet" id="dashicons-css" href="http://wordpress/wp-content/themes/fondsiluana/css/vasiliev.css" type="text/css">
 <body>
-	<div class="container c-block">
+<div class="container c-block">
 
-		<p>Здравствуйте! Вы можете пожертвовать денежные средства в благотворительный фонд Силуана Афонского и они пойдут на добрые дела!</p>
-		<form action="">
-			<input type="number" class="px-3 form-control fs-14 brd-inp brd-gld" placeholder="Введите сумму, руб">
+<div class="txt-desc text-left mb-3">
+Здравствуйте! Вы можете пожертвовать денежные средства в благотворительный фонд Силуана Афонского и они пойдут на добрые дела!
+</div>
 
-			<div class="row pt-3">
+<form>
+<div class="cont">
+<input type="text" class="input-pole" required>
+<div class="placeholder">Введите сумму в рублях</div>
+</div>
+</form>
 
-				<div class="col-sm">
-					<div class="crd my-2 text-center py-4">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/master-card.png" class="d-block img-card-t">
-						<div class="fs-14 mx-3 mt-2">Банковская платежная квитанция</div>
-					</div>
-				</div>
 
-				<div class="col-sm">
-					<div class="crd my-2 text-center py-4">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/master-card.png" class="d-block img-card-t">
-						<div class="fs-14 mx-3 mt-2">Банковская платежная квитанция</div>
-					</div>
-				</div>
+<div class="row d-flex justify-content-center my-3">
 
-				<div class="col-sm">
-					<div class="crd my-2 text-center py-4">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/master-card.png" class="d-block img-card-t">
-						<div class="fs-14 mx-3 mt-2">Банковская платежная квитанция</div>
-					</div>
-				</div>
+<div class="col-sm">
+<div class="crd my-3">
+<div class="card-body mx-auto">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/240px-MasterCard_early_1990s_logo.svg.png" class="d-block img-card-t">
+<div class="txt-desc">Банковская платежная квитанция</div>
+</div>
+</div>
+</div>
 
-			</div>
+<div class="col-sm">
+<div class="crd my-3">
+<div class="card-body mx-auto">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/240px-MasterCard_early_1990s_logo.svg.png" class="d-block img-card-t">
+<div class="txt-desc">Банковская платежная квитанция</div>
+</div>
+</div>
+</div>
 
-			<input type="email" class="px-3 form-control fs-14 brd-inp brd-gld my-3" placeholder="Ваш E-Mail" required>
-			<input type="text" class="px-3 form-control fs-14 brd-inp brd-gld my-3" placeholder="Имя и Фамилия" required>
+<div class="col-sm">
+<div class="crd my-3">
+<div class="card-body mx-auto">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/240px-MasterCard_early_1990s_logo.svg.png" class="d-block img-card-t">
+<div class="txt-desc">Банковская платежная квитанция</div>
+</div>
+</div>
+</div>
 
-			<div class="mb-3 mt-5 pt-2 gold-line-top position-relative">
-				<div>
-					<input class="form-check-input" type="checkbox" id="defaultCheck1" checked="">
-					<label class="form-check-label" for="defaultCheck1">
-						Я принимаю <a href="#" targer="_blank" rel="nofollow">Договор оферты</a>
-					</label>
-				</div> 
-				<div>
-					<input class="form-check-input" type="checkbox" id="defaultCheck2" checked="">
-					<label class="form-check-label pt-2" for="defaultCheck2">
-						Я принимаю <a href="#" targer="_blank" rel="nofollow">Соглашение об использовании персональных данных</a>
-					</label>
-				</div>
-			</div>
+</div>
 
-			<div class="row justify-content-center">
-				<div class="col-md-6 position-relative">
-					<a href="#" class="w-100 text-white hrt-btn fs-10 btn" tabindex="-1" role="button" aria-disabled="true">Пожертвовать</a>
-				</div>
-			</div>
+<div class="mx-auto justify-content-center">
+<form class="mt-3">
+<div class="cont">
+<input type="text" class="input-pole" required>
+<div class="placeholder">Ваш E-Mail <span class="red">*</span></div>
+</div>
+</form>
 
-		</form>
+<form class="mt-3">
+<div class="cont">
+<input type="text" class="input-pole" required>
+<div class="placeholder">Имя и Фамилия <span class="red">*</span></div>
+</div>
+</form>
+</div>
 
-	</div>
+<div class="fc golden-line-tr">
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label class="f-c-l" for="defaultCheck1">
+    Я принимаю <a href="" targer="_blank" rel="nofollow">Договор оферты</a>
+  </label>
+</div>
+
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+  <label class="f-c-l" for="defaultCheck2">
+    Я принимаю <a href="" targer="_blank" rel="nofollow">Соглашение об использовании персональных данных</a>
+  </label>
+</div>
+</div>
+
+<div class="d-flex justify-content-center">
+<a href="#ebat" class="w-50 text-white roboto btn fs-10 btn-red" tabindex="-1" role="button" aria-disabled="true">Пожертвовать</a>
+</div>
+
+</div>
 </body>
 
 
